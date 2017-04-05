22dc5ccb895bf37bbc74966a475d40f92eb95fba
Michael <mishkulik@gmail.com>
2017-04-05 23:14:18 +0700

    created helper

--- app/code/local/IGN/Siteblocks/controllers/TestController.php
+++ app/code/local/IGN/Siteblocks/controllers/TestController.php
@@ -4,6 +4,13 @@ class IGN_Siteblocks_TestController extends Mage_Core_Controller_Front_Action
 {
     public function myactionAction()
     {
-        die('hello');
+//        $block = Mage::getModel('siteblocks/block')->load(1);
+        $my = $this->getRequest()->getParams();
+        var_dump(Mage::helper('siteblocks')->isModuleOutputEnabled());
+        If (Mage::helper('siteblocks')->isModuleOutputEnabled()) {
+            echo 'Модуль включён';
+        } else {
+            echo 'Модуль выключен';
+        }
     }
 }
